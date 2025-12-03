3581072261fbfc3b695f31ee88b635abed9f1bb3
pokebase-debug <pokebase@pokebase.co>
2025-12-03 16:24:48 +0300

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main>
+    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
       <h1>PokeBase</h1>
       <p>Shop will be here.</p>
     </main>
